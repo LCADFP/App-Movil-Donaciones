c61348b0ca549f72ed0a894d7ba4b1221d7cf3fd
dafp18 <dafp18@hotmail.com>
2018-11-18 18:01:17 -0500

    Final vista tipsdonacion, se adicionan la page de paso a paso fundaciones
    Final vista tipsdonacion, se adicionan la page de paso a paso fundaciones

---
+++ src/pages/pasoapasofundacion/pasoapasofundacion.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { IonicPageModule } from 'ionic-angular';
+import { PasoapasofundacionPage } from './pasoapasofundacion';
+
+@NgModule({
+  declarations: [
+    PasoapasofundacionPage,
+  ],
+  imports: [
+    IonicPageModule.forChild(PasoapasofundacionPage),
+  ],
+})
+export class PasoapasofundacionPageModule {}
---
+++ src/pages/pasoapasofundacion/pasoapasofundacion.ts
@@ -0,0 +1,25 @@
+import { Component } from '@angular/core';
+import { IonicPage, NavController, NavParams } from 'ionic-angular';
+
+/**
+ * Generated class for the PasoapasofundacionPage page.
+ *
+ * See https://ionicframework.com/docs/components/#navigation for more info on
+ * Ionic pages and navigation.
+ */
+
+@IonicPage()
+@Component({
+  selector: 'page-pasoapasofundacion',
+  templateUrl: 'pasoapasofundacion.html',
+})
+export class PasoapasofundacionPage {
+
+  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  }
+
+  ionViewDidLoad() {
+    console.log('ionViewDidLoad PasoapasofundacionPage');
+  }
+
+}
